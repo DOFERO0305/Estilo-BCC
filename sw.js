9f9b99ed879327fbec1973a3de489360a8e9b6f0
DOFERO0305 <donald.fer.fer@gmail.com>
2026-05-03 13:00:56 +0200

    Implement service worker for caching resources

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'noticia365-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Retorna do cache se encontrar, senão vai à internet
+        return response || fetch(event.request);
+      })
+  );
+});
